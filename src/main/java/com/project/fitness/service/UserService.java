59a86cc996c348c3d87d3af2aca454ebde3c1140
pawan-exe <pawangupta63431@gmail.com>
2026-01-25 19:49:44 +0530

    Added dto layer

--- src/main/java/com/project/fitness/service/UserService.java
+++ src/main/java/com/project/fitness/service/UserService.java
@@ -1,16 +1,36 @@
 package com.project.fitness.service;
 
+import com.project.fitness.dto.RegisterUser;
+import com.project.fitness.dto.UserResponse;
 import com.project.fitness.model.User;
 import com.project.fitness.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
-    public User register(User user) {
-        return userRepository.save(user);
+    public UserResponse register(RegisterUser registerUser) {
+        User user = new User(null, registerUser.getEmail(), registerUser.getPassword(), registerUser.getFirstName(), registerUser.getLastName(), Instant.parse("2007-12-03T10:15:30.208Z").atZone(ZoneOffset.UTC).toLocalDateTime(), Instant.parse("2007-12-03T10:15:30.208Z").atZone(ZoneOffset.UTC).toLocalDateTime(), List.of(), List.of());
+        User savedUser = userRepository.save(user);
+        return mappedToResponse(savedUser);
+    }
+
+    public UserResponse mappedToResponse(User savedUser){
+        UserResponse response = new UserResponse();
+        response.setId(savedUser.getId());
+        response.setEmail(savedUser.getEmail());
+        response.setPassword(savedUser.getPassword());
+        response.setFirstName(savedUser.getFirstName());
+        response.setLastName(savedUser.getLastName());
+        response.setCreatedAt(savedUser.getCreatedAt());
+        response.setUpdatedAt(savedUser.getUpdatedAt());
+        return response;
     }
 }
